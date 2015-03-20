71af4f42389241794bd31216ba05c45bd3de9c29
frasator <frasator@gmail.com>
2015-03-20 15:55:50 +0100

    updates

--- src/cm-config.js
+++ src/cm-config.js
@@ -5,7 +5,7 @@ CELLBASE_HOST = "http://wwwdev.ebi.ac.uk/cellbase/webservices/rest";
 
 //OPENCGA_HOST = "http://ws-beta.bioinfo.cipf.es/opencga-staging-cellmaps/rest";
 //OPENCGA_HOST = "http://mem18:8080/opencga/rest";
-//OPENCGA_HOST = "http://test.babelomics.org/opencga/rest";
-OPENCGA_HOST = "http://ws.babelomics.org/opencga/rest";
+OPENCGA_HOST = "http://test.babelomics.org/opencga/rest";
+//OPENCGA_HOST = "http://ws.babelomics.org/opencga/rest";
 
 STUDY_NAME = "WorkSpace";
