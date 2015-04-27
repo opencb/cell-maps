04bf2451dcbbadb3233437819102b8d93d9bceee
frasator <frasator@gmail.com>
2015-04-27 14:50:04 +0200

    updated bower dependencies

--- build.sh
+++ build.sh
@@ -4,14 +4,9 @@ rm -rf build/index.html build/index.js build/workers build/fonts build/example-f
 
 vulcanize cell-maps-index.html -o build/index.html --strip --csp --inline
 
-cp -r bower_components/fontawesome/fonts build/
+cp -r lib/jsorolla/bower_components/fontawesome/fonts build/
 cp -r src/fonts/*.woff* build/fonts/
 cp -r example-files build/
 
-mkdir -p build/workers
-cp lib/jsorolla/src/lib/worker-fileupload.js build/workers/
-
-sed -i s@WORKERS_PATH=\"lib/jsorolla/src/lib/\"@WORKERS_PATH=\"workers/\"@g build/index.js
-
-sed -i s@../bower_components/fontawesome/fonts/fontawesome-webfont.@fonts/fontawesome-webfont.@g build/index.html
+sed -i s@../lib/jsorolla/bower_components/fontawesome/fonts/fontawesome-webfont.@fonts/fontawesome-webfont.@g build/index.html
 sed -i s@../src/fonts/@fonts/@g build/index.html
