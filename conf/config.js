2cd9f4457c662ae7e6798521f584827329ccf2b8
frasator <frasator@gmail.com>
2015-07-20 11:27:38 +0200

    updated bower, work in progress

---
+++ conf/config.js
@@ -0,0 +1,20 @@
+/**
+ * This is a configuration file.
+ * Changes to this file may cause the application does not work as it should
+ **/
+
+// Default
+OPENCGA_VERSION = "v1";
+OPENCGA_HOST = "http://ws.babelomics.org/opencga";
+
+CELLBASE_VERSION = "v3";
+CELLBASE_HOST = "http://bioinfodev.hpc.cam.ac.uk/cellbase";
+
+// Development
+OPENCGA_VERSION = "v1";
+OPENCGA_HOST = "http://test.babelomics.org/opencga";
+
+CELLBASE_VERSION = "v3";
+CELLBASE_HOST = "http://bioinfodev.hpc.cam.ac.uk/cellbase";
+
+STUDY_NAME = "CHECK_THIS";
